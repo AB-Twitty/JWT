2d0d9bdb16d5e6c840811d7e4caa0ee4c7302b35
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2023-09-07 13:58:58 +0300

    Building the internal structure

---
+++ JWT.Auth/JWTAuthenticationServicesRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JWT.Auth
+{
+	public static class JWTAuthenticationServicesRegistration
+	{
+		public static IServiceCollection ConfigureJWTAuthenticationServices (this IServiceCollection services, IConfiguration configuration)
+		{
+
+
+			return services;
+		} 
+	}
+}
